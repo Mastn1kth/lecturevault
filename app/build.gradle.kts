39fe4ad9640698157061b48a3901e07a768c7665
LectureVault Local <lecturevault@local>
2026-09-16 01:11:01 +0300

    release: prepare version 1.5.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.lecturevault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.5.8"
+        versionCode = 13
+        versionName = "1.5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
